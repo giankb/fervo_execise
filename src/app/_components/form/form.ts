1c9fa544a0fc859cc66e75fb2b4651d00954803b
DESKTOP-120F7OB\Win <gianlucabattisti96@gmail.com>
2025-11-27 00:00:54 +0100

    feat(form) form component generated

---
+++ src/app/_components/form/form.ts
@@ -0,0 +1,75 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+import {LoadingService} from '../../_services/loading.service';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FloatLabel} from 'primeng/floatlabel';
+import {NgClass} from '@angular/common';
+import {HomeService} from '../../_services/home.service';
+import {Toast} from 'primeng/toast';
+import {MessageService} from 'primeng/api';
+import {MessageType} from '../../_models/_enums/messageType';
+import {AutoComplete, AutoCompleteCompleteEvent, AutoCompleteSelectEvent} from 'primeng/autocomplete';
+import {GeocodingResult} from '../../_models/_types/geocodingResults';
+import {NotificationExtension} from '../../_models/_extensions/notification.extension';
+
+@Component({
+  selector: 'app-form',
+  imports: [
+    ReactiveFormsModule,
+    FloatLabel,
+    NgClass,
+    Toast,
+    AutoComplete,
+    FormsModule,
+  ],
+  templateUrl: './form.html',
+  styleUrl: './form.scss',
+  standalone: true
+})
+export class Form extends NotificationExtension{
+
+  @Output() locationSelected = new EventEmitter<GeocodingResult>();
+
+  selectedPlace: GeocodingResult | null = null;
+  filteredPlaces: GeocodingResult[] = [];
+  debounceTimer: any;
+
+  constructor(
+    private loadingService: LoadingService,
+    private homeService: HomeService,
+    messageService: MessageService
+  ) {
+    super(messageService);}
+
+  filterPlace(event: AutoCompleteCompleteEvent) {
+    const address = event.query;
+
+    clearTimeout(this.debounceTimer);
+    this.debounceTimer = setTimeout(() => {
+      if (!address || address.length < 3) return;
+
+      this.homeService.getPosition(address).subscribe({
+        next: data => {
+          this.filteredPlaces = data.results || [];
+        },
+        error: () => {
+          this.showMessage(MessageType.ERROR, 'Errore nel recupero dei dati');
+        }
+      });
+    }, 400);
+  }
+
+  getPlaceLabel(place: GeocodingResult | null): string {
+    return place ? `${place.name} - ${place.country}` : '';
+  }
+
+  selectPlace(event: AutoCompleteSelectEvent) {
+    this.selectedPlace = event.value;
+  }
+
+  sendLocation() {
+    if (!this.selectedPlace) return;
+    this.locationSelected.emit(this.selectedPlace);
+  }
+
+
+}
