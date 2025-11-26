464a750690fba7845c016846a0bd194f52e4d525
DESKTOP-120F7OB\Win <gianlucabattisti96@gmail.com>
2025-11-27 00:02:12 +0100

    feat(routes) routes update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {Home} from './_components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'home', component: Home },
+];
